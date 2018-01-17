fe7628a255888049e7fe56825170e871b5ea14b7
Douglas Cunha <douglas.cunha@ncr.com>
2018-01-17 11:14:47 -0200

    DSK-4681: Ajustes do review

--- 10/0008-cache.slot_rodizio(create).sql
+++ 10/0008-cache.slot_rodizio(create).sql
@@ -11,9 +11,6 @@ create table cache.slot_rodizio
   classe_id int null,
   material_id int null,
   descricao varchar(30) not null,
-  primary key clustered
-  (
-    venda_id, indice, slot_indice
-  ) with (pad_index = off, statistics_norecompute = off, ignore_dup_key = off, allow_row_locks = on, allow_page_locks = on) 
+  constraint pk_slot_rodizio primary key clustered (venda_id, indice, slot_indice) on [PRIMARY]
 ) 
 go
