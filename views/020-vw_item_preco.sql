3ccb052fccdd0374094d1f9214f6b188de0289e3
Pitty <pitty.d@ncr.com>
2018-02-05 20:39:46 -0200

    criada vw_item_preco para facilitar a consulta

---
+++ views/020-vw_item_preco.sql
@@ -0,0 +1,20 @@
+if object_id ('vw_item_preco') is not null
+  drop view vw_item_preco
+go
+
+create view [dbo].[vw_item_preco]
+as
+select 
+  t.id,
+  t.nome,
+  t.dt_vigencia,
+  t.modo_venda,
+  t.situacao,
+  i.material_id,
+  i.preco
+from vw_tabela_preco t
+join item_preco i on i.tabela_id = t.id
+where situacao = 'vigente'
+go
+
+
