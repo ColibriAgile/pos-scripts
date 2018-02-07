3cc290b7f8f56226185f3639d9188483ee12c235
Pitty <pitty.d@ncr.com>
2018-02-07 20:55:23 -0200

    Adicionada alteração de view em pasta de versao devido à dependencia de outra view

---
+++ 10/0012-vw_tabela_preco (+modo_venda_id).sql
@@ -0,0 +1,31 @@
+﻿if object_id ('vw_tabela_preco') is not null
+  drop view vw_tabela_preco
+go
+
+create view vw_tabela_preco
+as
+select 
+  id = t.id,
+  nome = t.nome,
+  t.dt_vigencia,
+  modo_venda_id = isnull(t.modo_venda, 0),
+  modo_venda = isnull(m.nome, '(geral)'),
+  situacao =   case  
+    when t.dt_vigencia = t2.dt_vigencia then 'vigente'
+    when t.dt_vigencia > getdate() then 'futura'
+    else 'expirada'
+  end
+from tabela_preco t
+left join modo_venda m on m.id = t.modo_venda
+join 
+(
+  select 
+    modo_venda_id = isnull(modo_venda,0), 
+    dt_vigencia = max(dt_vigencia)
+  from tabela_preco
+  where dt_vigencia <= getdate()
+  group by modo_venda
+) t2 ON
+  t2.modo_venda_id = isnull(t.modo_venda, 0)
+go
+
