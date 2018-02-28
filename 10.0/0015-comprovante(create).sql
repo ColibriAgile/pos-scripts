a0e75129aeb43c908c20ed26833cb837864e7f53
Pitty <pitty.d@ncr.com>
2018-02-28 19:39:52 -0300

    Removendo os TABs

--- 10.0/0015-comprovante(create).sql
+++ 10.0/0015-comprovante(create).sql
@@ -1,7 +1,8 @@
 if dbo.fn_existe('dbo.comprovante') = 1
   return
 
-create table dbo.comprovante (
+create table dbo.comprovante 
+(
   id int not null identity,
   operacao_id uniqueidentifier not null,
   consumidor varchar(30) null,
@@ -12,24 +13,28 @@ create table dbo.comprovante (
   constraint [pk_comprovante] primary key clustered (id) on [primary]
 ) on [primary]
 
-alter table dbo.comprovante  with check add foreign key([operacao_id])
-references [dbo].[operacao] ([operacao_id])
+alter table dbo.comprovante 
+with check add foreign key
+([operacao_id])
+references [dbo].[operacao] 
+([operacao_id])
+
 go
 
 if dbo.fn_existe('dbo.operacao.comprovante') = 1
   exec('
-	insert into comprovante
-	select 
-		o.operacao_id, 
-		consumidor = isnull(nullif(c.string_value, ''''),''(nao informado)''), 
-	    comprovante, 
-		comprovante_chave, 
-		comprovante_status, 
-		comprovante_ressalva
-	from operacao o 
-	left join operacao_venda ov on o.operacao_id = ov.operacao_id
-	cross apply dbo.fn_parse_json(isnull(nullif(consumidor, ''''),''{"cpf":"(nao informado)"}'')) c
-	where c.name = ''cpf''
+insert into comprovante
+select 
+	o.operacao_id, 
+	consumidor = isnull(nullif(c.string_value, ''''),''(nao informado)''), 
+	comprovante, 
+	comprovante_chave, 
+	comprovante_status, 
+	comprovante_ressalva
+from operacao o 
+left join operacao_venda ov on o.operacao_id = ov.operacao_id
+cross apply dbo.fn_parse_json(isnull(nullif(consumidor, ''''),''{"cpf":"(nao informado)"}'')) c
+where c.name = ''cpf''
 ')
 go
 
@@ -37,7 +42,8 @@ go
 if dbo.fn_existe('dbo.comprovante_geral') = 1
   return
 
-create table dbo.comprovante_geral (
+create table dbo.comprovante_geral 
+(
   id int not null identity,
   operacao_id uniqueidentifier not null,
   consumidor varchar(30) null,
@@ -48,22 +54,25 @@ create table dbo.comprovante_geral (
   constraint [pk_comprovante_geral] primary key clustered (id) on [primary]
 ) on [primary]
 
-alter table dbo.comprovante_geral with check add foreign key([operacao_id])
-references [dbo].[operacao_geral] ([operacao_id])
+alter table dbo.comprovante_geral 
+with check add foreign key
+([operacao_id])
+references [dbo].[operacao_geral] 
+([operacao_id])
 
 if dbo.fn_existe('dbo.operacao.comprovante_geral') = 1
-exec('
-	insert into comprovante_geral
-	select 
-	  o.operacao_id, 
-	  consumidor = isnull(nullif(c.string_value, ''''),''(nao informado)''), 
-	  comprovante, 
-	  comprovante_chave, 
-	  comprovante_status, 
-	  comprovante_ressalva
-	from operacao_geral o 
-	left join operacao_venda_geral ov on o.operacao_id = ov.operacao_id
-	cross apply dbo.fn_parse_json(isnull(nullif(consumidor, ''''),''{"cpf":"(nao informado)"}'')) c
-	where c.name = ''cpf''
+  exec('
+insert into comprovante_geral
+select 
+	o.operacao_id, 
+	consumidor = isnull(nullif(c.string_value, ''''),''(nao informado)''), 
+	comprovante, 
+	comprovante_chave, 
+	comprovante_status, 
+	comprovante_ressalva
+from operacao_geral o 
+left join operacao_venda_geral ov on o.operacao_id = ov.operacao_id
+cross apply dbo.fn_parse_json(isnull(nullif(consumidor, ''''),''{"cpf":"(nao informado)"}'')) c
+where c.name = ''cpf''
 ')
 go
