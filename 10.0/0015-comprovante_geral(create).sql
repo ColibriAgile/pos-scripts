3f18eac361656f5a1fcac6ae8ea3dcd453ec5182
Andre Foresti <andre.foresti@ncr.com>
2018-03-08 16:02:46 -0300

    FI-90 ajuste na criação da tabela comprovante_geral

--- 10.0/0015-comprovante_geral(create).sql
+++ 10.0/0015-comprovante_geral(create).sql
@@ -1,29 +1,28 @@
-if dbo.fn_existe('dbo.comprovante') = 1
-  return
+-- Geral
+if dbo.fn_existe('dbo.comprovante_geral') = 1
+  drop table dbo.comprovante_geral
 
-create table dbo.comprovante 
+create table dbo.comprovante_geral 
 (
-  id int not null identity,
+  id int not null,
   operacao_id uniqueidentifier not null,
   consumidor varchar(30) null,
   numero varchar(20) null,
   chave varchar(150) null,
   [status] varchar(20) null,
   ressalva varchar(250) null,
-  constraint [pk_comprovante] primary key clustered (id) on [primary]
+  constraint [pk_comprovante_geral] primary key clustered (id) on [primary]
 ) on [primary]
 
-alter table dbo.comprovante 
+alter table dbo.comprovante_geral 
 with check add foreign key
 ([operacao_id])
-references [dbo].[operacao] 
+references [dbo].[operacao_geral] 
 ([operacao_id])
 
-go
-
-if dbo.fn_existe('dbo.operacao.comprovante') = 1
+if dbo.fn_existe('dbo.operacao.comprovante_geral') = 1
   exec('
-insert into comprovante
+insert into comprovante_geral
 select 
 	o.operacao_id, 
 	consumidor = isnull(nullif(c.string_value, ''''),''(nao informado)''), 
@@ -31,10 +30,9 @@ select
 	comprovante_chave, 
 	comprovante_status, 
 	comprovante_ressalva
-from operacao o 
-left join operacao_venda ov on o.operacao_id = ov.operacao_id
+from operacao_geral o 
+left join operacao_venda_geral ov on o.operacao_id = ov.operacao_id
 cross apply dbo.fn_parse_json(isnull(nullif(consumidor, ''''),''{"cpf":"(nao informado)"}'')) c
 where c.name = ''cpf''
 ')
 go
-
