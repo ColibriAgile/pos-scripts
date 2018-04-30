afa6d61597726a73e891f880db09ee4dcf874bf9
Pitty <pitty.d@ncr.com>
2018-04-29 22:38:52 -0300

    corrigido script qdo há mais de um ponto de venda 'nenhum' na base

--- 10.0/0021-ponto_venda(delete nenhum).sql
+++ 10.0/0021-ponto_venda(delete nenhum).sql
@@ -1,8 +1,34 @@
-declare @id int = (select id from ponto_venda where lower(nome) = 'nenhum')
-update maquina set ponto_balcao_id = null where ponto_balcao_id = @id
-update maquina set ponto_entrega_id = null where ponto_entrega_id = @id
-update maquina set ponto_mesa_id = null where ponto_mesa_id = @id
-update maquina set ponto_ficha_id = null where ponto_ficha_id = @id
-update venda_item set ponto_venda_id = null where ponto_venda_id = @id
-update venda_item_geral set ponto_venda_id = null where ponto_venda_id = @id
-delete ponto_venda where nome = 'nenhum'
+declare @ids table (pdv_id integer)
+
+insert @ids
+(pdv_id)
+select id 
+from ponto_venda 
+where lower(nome) = 'nenhum'
+
+update maquina 
+set ponto_balcao_id = null
+where ponto_balcao_id in (select pdv_id from @ids)
+
+update maquina 
+set ponto_entrega_id = null 
+where ponto_entrega_id in (select pdv_id from @ids)
+
+update maquina 
+set ponto_mesa_id = null
+where ponto_mesa_id in (select pdv_id from @ids)
+
+update maquina 
+set ponto_ficha_id = null 
+where ponto_ficha_id in (select pdv_id from @ids)
+
+update venda_item 
+set ponto_venda_id = null 
+where ponto_venda_id in (select pdv_id from @ids)
+
+update venda_item_geral 
+set ponto_venda_id = null 
+where ponto_venda_id in (select pdv_id from @ids)
+
+delete ponto_venda 
+where nome = 'nenhum'
