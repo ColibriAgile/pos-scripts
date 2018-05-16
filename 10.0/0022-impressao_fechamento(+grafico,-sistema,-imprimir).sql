8f95d94c148e191daa3c9cc6e6a880681dc912c6
Pitty <pitty.d@ncr.com>
2018-05-15 21:42:16 -0300

    Endentaçao

--- 10.0/0022-impressao_fechamento(+grafico,-sistema,-imprimir).sql
+++ 10.0/0022-impressao_fechamento(+grafico,-sistema,-imprimir).sql
@@ -1,21 +1,26 @@
 exec dbo.sp_renomear 'dbo.impressao_fechamento.sistema', 'grafico', 'column'
 go
-delete from dbo.impressao_fechamento where imprimir=0
+
+delete from dbo.impressao_fechamento 
+where imprimir = 0
 go
+
 exec dbo.sp_apagar_campo 'dbo.impressao_fechamento', 'imprimir'
 go
-update impressao_fechamento set grafico = 0
+
+update impressao_fechamento 
+set grafico = 0
 go
 
 exec sp_apagar_pk 'dbo.impressao_fechamento'
 
 alter table dbo.impressao_fechamento add constraint
-	pk_impressao_fechamento primary key clustered 
-	(
+pk_impressao_fechamento primary key clustered 
+(
   tipo,
 	grafico,
   ordem,
 	nome
-	) with( statistics_norecompute = off, ignore_dup_key = off, allow_row_locks = on, allow_page_locks = on) on [primary]
+) with (statistics_norecompute = off, ignore_dup_key = off, allow_row_locks = on, allow_page_locks = on) on [primary]
 
 go
