1f71f04aa9c0dcf68762d0ab0206be1f7487fd04
Pitty <pitty.d@ncr.com>
2018-06-21 14:43:33 -0300

    adicionado indice na movimento caixa tb

--- finalizacao/600-indices.sql
+++ finalizacao/600-indices.sql
@@ -7,12 +7,27 @@ if not exists(
   from sysindexes
   where name = 'ix_operacao_geral$dt_contabil'
 ) 
-  create clustered index [ix_operacao_geral$dt_contabil] on [dbo].[operacao_geral]
+  create clustered index [ix_operacao_geral$dt_contabil] 
+  on [dbo].[operacao_geral]
   (
     [dt_contabil] ASC
   ) with (pad_index = off, statistics_norecompute = off, sort_in_tempdb = off, drop_existing = off, online = off, allow_row_locks = on, allow_page_locks = on)
 go
 
+/*-------------------------------------------------------------
+ movimento_caixa
+ -------------------------------------------------------------*/
+if not exists(
+  select * 
+  from sysindexes
+  where name = 'ix_movimento_caixa$turno_id'
+) 
+  create nonclustered index [ix_movimento_caixa$turno_id] 
+  on [dbo].[movimento_caixa]
+  (
+    [turno_id] ASC
+  ) with (pad_index = off, statistics_norecompute = off, sort_in_tempdb = off, drop_existing = off, online = off, allow_row_locks = on, allow_page_locks = on)
+go
 
 /*-------------------------------------------------------------
  movimento_caixa_geral 
@@ -22,8 +37,11 @@ if not exists(
   from sysindexes
   where name = 'ix_movimento_caixa_geral$turno_id'
 ) 
-  create index [ix_movimento_caixa_geral$turno_id] on [dbo].[movimento_caixa_geral]
+  create nonclustered index [ix_movimento_caixa_geral$turno_id] 
+  on [dbo].[movimento_caixa_geral]
   (
     [turno_id] ASC
   ) with (pad_index = off, statistics_norecompute = off, sort_in_tempdb = off, drop_existing = off, online = off, allow_row_locks = on, allow_page_locks = on)
 go
+
+
