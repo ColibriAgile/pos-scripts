3bd6b63db1bb9867d71d9ac1e476f680bb9f334c
Pitty <pitty.d@ncr.com>
2018-06-23 20:40:34 -0300

    indice por data contabil no turno

--- finalizacao/600-indices.sql
+++ finalizacao/600-indices.sql
@@ -2,46 +2,64 @@
 /*-------------------------------------------------------------
  operacao_geral 
  -------------------------------------------------------------*/
-if not exists(
+if not exists
+(
   select * 
   from sysindexes
   where name = 'ix_operacao_geral$dt_contabil'
 ) 
-  create clustered index [ix_operacao_geral$dt_contabil] 
-  on [dbo].[operacao_geral]
+  create clustered index ix_operacao_geral$dt_contabil 
+  on dbo.operacao_geral
   (
-    [dt_contabil] ASC
+    dt_contabil ASC
   ) with (pad_index = off, statistics_norecompute = off, sort_in_tempdb = off, drop_existing = off, online = off, allow_row_locks = on, allow_page_locks = on)
 go
 
 /*-------------------------------------------------------------
  movimento_caixa
  -------------------------------------------------------------*/
-if not exists(
+if not exists
+(
   select * 
   from sysindexes
   where name = 'ix_movimento_caixa$turno_id'
 ) 
-  create nonclustered index [ix_movimento_caixa$turno_id] 
-  on [dbo].[movimento_caixa]
+  create nonclustered index ix_movimento_caixa$turno_id 
+  on dbo.movimento_caixa
   (
-    [turno_id] ASC
+    turno_id ASC
   ) with (pad_index = off, statistics_norecompute = off, sort_in_tempdb = off, drop_existing = off, online = off, allow_row_locks = on, allow_page_locks = on)
 go
 
 /*-------------------------------------------------------------
  movimento_caixa_geral 
  -------------------------------------------------------------*/
-if not exists(
+if not exists
+(
   select * 
   from sysindexes
   where name = 'ix_movimento_caixa_geral$turno_id'
 ) 
-  create nonclustered index [ix_movimento_caixa_geral$turno_id] 
-  on [dbo].[movimento_caixa_geral]
+  create nonclustered index ix_movimento_caixa_geral$turno_id 
+  on dbo.movimento_caixa_geral
   (
-    [turno_id] ASC
+    turno_id ASC
   ) with (pad_index = off, statistics_norecompute = off, sort_in_tempdb = off, drop_existing = off, online = off, allow_row_locks = on, allow_page_locks = on)
 go
 
+/*-------------------------------------------------------------
+ turno
+ -------------------------------------------------------------*/
+if not exists
+(
+  select * 
+  from sysindexes
+  where name = 'ix_turno$dt_contabil'
+) 
+  create clustered index ix_turno$dt_contabil 
+  on dbo.turno
+  (
+    dt_contabil asc
+  ) with (pad_index = off, statistics_norecompute = off, sort_in_tempdb = off, drop_existing = off, online = off, allow_row_locks = on, allow_page_locks = on)
+go
 
