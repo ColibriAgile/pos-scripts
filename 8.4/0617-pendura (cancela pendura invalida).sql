a13a70149a04a21cc4dd3fda5650ba2c690966a3
Douglas Cunha <douglas.cunha@ncr.com>
2018-08-06 16:48:04 -0300

    LAB-9:Pequeno ajuste no script de ajuste de pendura.

--- 8.4/0617-pendura (cancela pendura invalida).sql
+++ 8.4/0617-pendura (cancela pendura invalida).sql
@@ -6,20 +6,15 @@ where pendura_id in
 (
   select p.pendura_id
   from pendura as p 
-  left join movimentocaixa as m on 
-    m.operacao_id = p.operacao_id and
-    m.vl_recebido = abs(p.vl_total)
-  where vl_total < 0
-    and vl_recebido is null
-    and p.cancelado = 0
-  union
-  select p.pendura_id
-  from pendura as p 
   left join movimentocaixageral as m on 
     m.operacao_id = p.operacao_id and
     m.vl_recebido = abs(p.vl_total)
-  where vl_total < 0
-    and vl_recebido is null
+  left join movimentocaixa as md on 
+    md.operacao_id = p.operacao_id and
+    md.vl_recebido = abs(p.vl_total)
+  where p.vl_total < 0
+    and md.vl_recebido is null
+    and md.vl_recebido is null
     and p.cancelado = 0
 )
 go
