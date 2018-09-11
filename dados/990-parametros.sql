81343a18141a64783b64c6cfd4ab297b2d620eb2
Andre Foresti <andre.foresti@ncr.com>
2018-09-11 11:56:53 -0300

    DSK-5095 ajuste sugerido no review +review RDSK-133

--- dados/990-parametros.sql
+++ dados/990-parametros.sql
@@ -845,9 +845,9 @@ exec sp_inserir_parametro
   @codigo = 'CfgPedeAtendenteNaVenda',
   @categoria = 'Modo',
   @subcategoria = 'Lançamento',
-  @descr = 'Pede atendente na venda',
+  @descr = 'Pede atendente no lançamento quando o terminal também está configurado para tal.',
   @detalhes = 'Quando ativado solicita a escolha de um atendente ao fazer um lançamento. Útil para quando o lançamento é feito pelo caixa a pedido do garçom.
-Este parâmetro deve ser utilizado em conjunto com o parâmetro "Nº do atendente na venda" nas configurações do terminal.',
+Para este parâmetro funcionar deve-se ativar também o parâmetro "Nº do atendente na venda" nas configurações do terminal.',
   @tipo_valor = 'boolean',
   @tipo_param = 'V',
   @valor_default = '0',
