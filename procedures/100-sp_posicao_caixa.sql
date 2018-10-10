b8be4066f3a2583d5d2ad30bbca550b5df890f30
Andrey Santanna <andrey.santanna@ncr.com>
2018-10-10 15:11:53 -0300

    DSK-5099 (ncr-44929) Sistema não obedece a permissao de efetuar sangria acima do valor do caixa

--- procedures/100-sp_posicao_caixa.sql
+++ procedures/100-sp_posicao_caixa.sql
@@ -5,41 +5,41 @@ go
 create procedure sp_posicao_caixa (@maquina_id int, @func_id int)
 as
 
-	declare @troco_dinheiro money;
-	declare @troco_contravale money;
+  declare @troco_dinheiro money;
+  declare @troco_contravale money;
 
-	set @troco_dinheiro =
-	  (select isnull(sum(vl_recebido),0)
-	   from dbo.movimento_caixa m with(nolock)
-	   where (m.maquina_id = @maquina_id)
-     and (m.func_recebeu_id = @func_id)
-		 and (isnull(m.cancelado, 0) = 0)
-		 and meio_pagamento_id = -1)
-
-
-	set @troco_contravale =
-	  (select isnull(sum(vl_recebido),0)
-	   from dbo.movimento_caixa m with(nolock)
-	   where (m.maquina_id = @maquina_id)
-     and (m.func_recebeu_id = @func_id)
-		 and (isnull(m.cancelado, 0) = 0)
-		 and meio_pagamento_id = -3)
+  set @troco_dinheiro =
+    (select isnull(sum(vl_recebido),0)
+     from dbo.movimento_caixa m with(nolock)
+     where (m.maquina_id = @maquina_id)
+       and (m.func_recebeu_id = @func_id)
+       and (isnull(m.cancelado, 0) = 0)
+       and meio_pagamento_id = -1)
 
 
-	select
-	  id = m.meio_pagamento_id,
-	  valor = isnull(sum(vl_recebido),0) -
-		case meio_pagamento_id
-		  when 1 then @troco_dinheiro
-		  when 3 then @troco_contravale
-		  else 0
-		end
-	  from movimento_caixa m with(nolock)
-	where (m.maquina_id = @maquina_id)
+  set @troco_contravale =
+    (select isnull(sum(vl_recebido),0)
+     from dbo.movimento_caixa m with(nolock)
+     where (m.maquina_id = @maquina_id)
+       and (m.func_recebeu_id = @func_id)
+       and (isnull(m.cancelado, 0) = 0)
+       and meio_pagamento_id = -3)
+
+
+  select
+    id = m.meio_pagamento_id,
+    valor = isnull(sum(vl_recebido),0) -
+    case meio_pagamento_id
+      when 1 then @troco_dinheiro
+      when 3 then @troco_contravale
+      else 0
+    end
+  from movimento_caixa m with(nolock)
+  where (m.maquina_id = @maquina_id)
     and (m.func_recebeu_id = @func_id)
-	  and (isnull(m.cancelado, 0) = 0)
-	  and meio_pagamento_id > 0
-	  group by m.meio_pagamento_id
+    and (isnull(m.cancelado, 0) = 0)
+    and meio_pagamento_id > 0
+  group by m.meio_pagamento_id
 
 go
 
