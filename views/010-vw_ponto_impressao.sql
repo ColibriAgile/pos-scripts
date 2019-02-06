8f4f02208e2804a374e78f12260a231ff9dd27ac
Andrey Santanna <andrey.santanna@ncr.com>
2019-02-06 09:52:45 -0200

    PM-19 O monitor deve utilizar o ponto de impressão do .RENDER sem obrigar o cadastro de um perfil de impressão

---
+++ views/010-vw_ponto_impressao.sql
@@ -0,0 +1,21 @@
+﻿if object_id('vw_ponto_impressao') is not null
+  drop view [dbo].[vw_ponto_impressao]
+go
+
+create view [dbo].[vw_ponto_impressao]
+as
+
+select
+  ponto_impressao_id = po.id,
+  ponto_impressao = po.nome,
+  impressora_id,
+  impressora = imp.nome,
+  po.porta,
+  po.impressora_backup_id,
+  impressora_backup = impbak.nome,
+  po.porta_backup
+from dbo.ponto_impressao po
+left join dbo.impressora imp on imp.id = po.impressora_id
+left join dbo.impressora impbak on impbak.id = po.impressora_backup_id
+
+go
