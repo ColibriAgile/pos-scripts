85ef087ddda042a937a0c2c75edc36e275f69942
Andrey Santanna <andrey.santanna@ncr.com>
2019-02-12 15:11:02 -0200

    DSK-5259 Introduzir o conceito de plugin global na arquitetura de plugins

--- functions/000-fn_plugin_maquina.sql
+++ functions/000-fn_plugin_maquina.sql
@@ -2,24 +2,34 @@ if object_id ('fn_plugin_maquina') is not null
   drop function fn_plugin_maquina
 go
 
-create function fn_plugin_maquina (@plugin varchar(100), @sistema bit = 0)
-returns table
-as
-return
+create function fn_plugin_maquina (@plugin varchar(100), @sistema bit = 0, @global bit = 0)
+returns @tbl table
 (
-  select
-    ativo = cast(
-      case @sistema
-        when 0 then isnull(p.ativo, 0)
-        else 1
-      end as bit
-    ),
-    maquina_id = id,
-    maquina = m.nome,
-    plugin = p.nome
-  from maquina m
-  left join plugin_maquina p on 
-    p.maquina_id = m.id and
-    lower(p.nome) = lower(@plugin)
+  ativo bit,
+  maquina_id int,
+  maquina nvarchar(20),
+  plugin varchar(100)
 )
+as
+begin
+  declare
+	@ativo bit
+
+  if @sistema = 1
+	select @ativo= 1
+  else if @global = 1
+	select @ativo= isnull((select ativo from plugin_maquina where maquina_id=-1 and nome=@plugin), 0)
+
+  insert into @tbl
+    select
+      ativo = cast(isnull(@ativo, isnull(p.ativo, 0)) as bit),
+      maquina_id = id,
+      maquina = m.nome,
+      plugin = @plugin
+    from maquina m
+    left join plugin_maquina p on 
+      p.maquina_id = m.id and
+      lower(p.nome) = lower(@plugin)
+	return
+end
 go
