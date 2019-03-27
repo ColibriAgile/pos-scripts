ae1917132ecd3d906e1e2fbaf4f9a91e0bb57824
Andrey Santanna <andreysantanna@gmail.com>
2019-03-27 11:45:05 -0300

    DSK-5286 Implementar conceito de plugin 'sempre ativo'

--- procedures/001-sp_ativar_plugin.sql
+++ procedures/001-sp_ativar_plugin.sql
@@ -5,17 +5,28 @@ go-------------------------------------------------------
 create procedure [dbo].[sp_ativar_plugin](
   @maquina_id int,
   @nome varchar(50),
-  @arquivo varchar(200)
+  @arquivo varchar(200),
+  @ativar int = 1
 ) as
 begin
-  -- Se estou desativando (maq zero, ou ativando um plugin global, negativos, removo os ativos)
-  if @maquina_id <= 0
-	delete from dbo.plugin_maquina where nome = @nome and arquivo = @arquivo;
-  else
-    delete from dbo.plugin_maquina where nome = @nome and arquivo = @arquivo and 
-	(maquina_id = @maquina_id or maquina_id < 0);
+  declare @estado int = 0;
+  select @estado=maquina_id from dbo.plugin_maquina where nome = @nome and maquina_id < 0
+  
+  -- Apenas proteção
+  if @maquina_id = 0 
+	return
 
-  if @maquina_id <> 0
+  -- Não posso mudar um estado negativo... apenas desativá-lo
+  if @estado < 0 and (@maquina_id <> @estado or @ativar = 1)
+	return
+
+  -- Se estou desativando (maq zero, ou ativando um plugin global, negativos, removo os ativos)
+  if @maquina_id > 0
+    delete from dbo.plugin_maquina where nome = @nome and maquina_id = @maquina_id;
+  else
+	delete from dbo.plugin_maquina where nome = @nome;
+
+  if @ativar = 1
 	insert plugin_maquina (ativo, nome, arquivo, maquina_id)
 	  values (1, @nome, @arquivo, @maquina_id)
 end
