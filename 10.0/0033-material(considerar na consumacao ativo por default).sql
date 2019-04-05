e1e194ad14321e1a73591c4509bb6cdf18b0373b
Andrey Santanna <andrey.santanna@ncr.com>
2019-04-05 17:15:21 -0300

    DSK-5292 Campo de considerar consumação devia ser default 1

--- 10.0/0033-material(considerar na consumacao ativo por default).sql
+++ 10.0/0033-material(considerar na consumacao ativo por default).sql
@@ -1,1 +1,8 @@
-update dbo.material set consumacao=1
+update dbo.material
+  set consumacao=1
+go
+
+alter table dbo.material
+  add constraint df_material$consumacao 
+  default 1 for consumacao
+go
