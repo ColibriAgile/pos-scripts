a2b22eef9d05b33578d9766d34fba5bfbd5b1760
Pitty <pitty.d@ncr.com>
2019-05-09 22:35:12 -0300

    Endentação

--- 8.6/0003-popular turno_id da movimento_caixa.sql
+++ 8.6/0003-popular turno_id da movimento_caixa.sql
@@ -1,24 +1,35 @@
-declare @turnos table (mov_id int, turno_id int)
+begin tran
+
+declare @turnos table 
+(
+  mov_id int not null, 
+  turno_id int not null
+)
 
 insert @turnos
 select 
   m.movimento_caixa_id,
-  isnull(c.turno_id, -1)
-from (
+  isnull(t.turno_id, -1)
+from 
+(
   select
     c.movimento_caixa_id,
     c.dt_hr_pagamento,
     c.func_recebeu_id
   from dbo.movimento_caixa c 
   where c.turno_id is null
+
   union
+
   select
     c.movimento_caixa_id,
     c.dt_hr_pagamento,
     c.func_recebeu_id
   from dbo.pre_pagamento c 
   where c.turno_id is null
+
   union
+
   select 
     c.movimento_caixa_id,
     c.dt_hr_pagamento,
@@ -26,22 +37,22 @@ from (
   from dbo.movimento_caixa_geral c
   where c.turno_id is null
 ) m
-left join turno c on c.func_id = m.func_recebeu_id 
-  and m.dt_hr_pagamento between c.dt_hr_abertura and isnull(c.dt_hr_fechamento, getdate())
+left join dbo.turno t on 
+  t.func_id = m.func_recebeu_id and 
+  m.dt_hr_pagamento between t.dt_hr_abertura and isnull(t.dt_hr_fechamento, getdate())
 
-
-update dbo.movimento_caixa set
-  turno_id = t.turno_id
+update dbo.movimento_caixa 
+set turno_id = t.turno_id
 from @turnos t
 where t.mov_id = movimento_caixa.movimento_caixa_id 
 
-update dbo.pre_pagamento set
-  turno_id = t.turno_id
+update dbo.pre_pagamento 
+set turno_id = t.turno_id
 from @turnos t
 where t.mov_id = pre_pagamento.movimento_caixa_id 
 
-update dbo.movimento_caixa_geral set
-  turno_id = t.turno_id
+update dbo.movimento_caixa_geral 
+set turno_id = t.turno_id
 from @turnos t
 where t.mov_id = movimento_caixa_geral.movimento_caixa_id
 
@@ -53,7 +64,4 @@ alter table dbo.movimento_caixa_geral
 
 alter table dbo.pre_pagamento
   alter column turno_id int not null
-
-
-
 
