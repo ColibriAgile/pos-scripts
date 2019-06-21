6a9dc305776553f2ae4b025e1e8d54417d18b394
Andre <andre.foresti@ncr.com>
2019-06-21 13:10:31 -0300

    verifica a versão do arquivo e executa um merge diferente para versões anteriores a 230000 +review RSY-22

--- procedures/900-sync.sp_consolidar.sql
+++ procedures/900-sync.sp_consolidar.sql
@@ -16,13 +16,6 @@ as
 begin
   set nocount on
 
-  if @versao < 230000
-    RAISERROR (  
-'***********************************************************************
- A versão do arquivo de cadastros não é mais suportada pelo ColibriSync.
- Atualize a versão do Colibri Back Office na retaguarda, e envie os arquivos novamente.
-***********************************************************************', 15, 1);
-
   declare
     @loja_id int,
     @rede_id int,
@@ -176,83 +169,162 @@ begin
   begin try
     set identity_insert dbo.material on
 
-    merge dbo.material as target
-    using sync.material as source with (nolock) on 
-      target.id = source.material_id
-    when matched then
-      update set
-        --descricao_touch = source.descricao_touch,
-        --vende_combo = source.bn_vendenocombo,
-        ativo = source.bn_ativo,
-        dt_alt = @getDate,
-        codigo = source.nu_codigo,
-        descricao = source.nm_descricao,
-        descricao_producao = source.nm_descricaoproducao,
-        tecla_prog = source.nm_teclaprog,
-        imagem = source.nm_figura,
-        cod_externo = source.cod_externo,
-        unidade = source.unid_venda,
-        venda = source.bn_venda,
-        consumacao = source.bn_consumacao,
-        servico = source.bn_servico,
-        requer_obs = source.bn_requerobs,
-        qtde_frac = source.bn_qtdefrac,
-        balanca = source.bn_balanca,
-        grupo_id = source.grupo_id
-    when not matched by target then
-      insert
-      (
-        id,
-        ativo,
-        dt_alt,
-        codigo,
-        descricao,
-        descricao_touch,
-        descricao_producao,
-        tecla_prog,
-        imagem,
-        cod_externo,
-        unidade,
-        venda,
-        servico,
-        requer_obs,
-        qtde_frac,
-        balanca,
-        consumacao,
-        vende_combo,
-        vende_web,
-        grupo_id,
-        loja_id,
-        rede_id,
-        local_producao_id
-      ) values
-      (
-        source.material_id,
-        source.bn_ativo,
-        @getDate,
-        source.nu_codigo,
-        source.nm_descricao,
-        source.nm_descricaotouch,
-        source.nm_descricaoproducao,
-        source.nm_teclaprog,
-        source.nm_figura,
-        source.cod_externo,
-        source.unid_venda,
-        source.bn_venda,
-        source.bn_servico,
-        source.bn_requerobs,
-        source.bn_qtdefrac,
-        source.bn_balanca,
-        source.bn_consumacao,
-        0, --source.bn_vendenocombo,
-        0,
-        source.grupo_id,
-        @loja_id,
-        @rede_id,
-        source.st_localimpressao
-      )
-    when not matched by source then
-      update set target.ativo = 0;
+    if @versao < 230000
+    begin
+      merge dbo.material as target
+      using sync.material as source with (nolock) on 
+        target.id = source.material_id
+      when matched then
+        update set
+          --descricao_touch = source.descricao_touch,
+          --vende_combo = source.bn_vendenocombo,
+          ativo = source.bn_ativo,
+          dt_alt = @getDate,
+          codigo = source.nu_codigo,
+          descricao = source.nm_descricao,
+          descricao_producao = source.nm_descricaoproducao,
+          tecla_prog = source.nm_teclaprog,
+          imagem = source.nm_figura,
+          cod_externo = source.cod_externo,
+          unidade = source.unid_venda,
+          venda = source.bn_venda,
+          servico = source.bn_servico,
+          requer_obs = source.bn_requerobs,
+          qtde_frac = source.bn_qtdefrac,
+          balanca = source.bn_balanca,
+          grupo_id = source.grupo_id
+      when not matched by target then
+        insert
+        (
+          id,
+          ativo,
+          dt_alt,
+          codigo,
+          descricao,
+          descricao_touch,
+          descricao_producao,
+          tecla_prog,
+          imagem,
+          cod_externo,
+          unidade,
+          venda,
+          servico,
+          requer_obs,
+          qtde_frac,
+          balanca,
+          vende_combo,
+          vende_web,
+          grupo_id,
+          loja_id,
+          rede_id,
+          local_producao_id
+        ) values
+        (
+          source.material_id,
+          source.bn_ativo,
+          @getDate,
+          source.nu_codigo,
+          source.nm_descricao,
+          source.nm_descricaotouch,
+          source.nm_descricaoproducao,
+          source.nm_teclaprog,
+          source.nm_figura,
+          source.cod_externo,
+          source.unid_venda,
+          source.bn_venda,
+          source.bn_servico,
+          source.bn_requerobs,
+          source.bn_qtdefrac,
+          source.bn_balanca,
+          0, --source.bn_vendenocombo,
+          0,
+          source.grupo_id,
+          @loja_id,
+          @rede_id,
+          source.st_localimpressao
+        )
+      when not matched by source then
+        update set target.ativo = 0;
+    end else
+    begin
+      merge dbo.material as target
+      using sync.material as source with (nolock) on 
+        target.id = source.material_id
+      when matched then
+        update set
+          --descricao_touch = source.descricao_touch,
+          --vende_combo = source.bn_vendenocombo,
+          ativo = source.bn_ativo,
+          dt_alt = @getDate,
+          codigo = source.nu_codigo,
+          descricao = source.nm_descricao,
+          descricao_producao = source.nm_descricaoproducao,
+          tecla_prog = source.nm_teclaprog,
+          imagem = source.nm_figura,
+          cod_externo = source.cod_externo,
+          unidade = source.unid_venda,
+          venda = source.bn_venda,
+          consumacao = source.bn_consumacao,
+          servico = source.bn_servico,
+          requer_obs = source.bn_requerobs,
+          qtde_frac = source.bn_qtdefrac,
+          balanca = source.bn_balanca,
+          grupo_id = source.grupo_id
+      when not matched by target then
+        insert
+        (
+          id,
+          ativo,
+          dt_alt,
+          codigo,
+          descricao,
+          descricao_touch,
+          descricao_producao,
+          tecla_prog,
+          imagem,
+          cod_externo,
+          unidade,
+          venda,
+          servico,
+          requer_obs,
+          qtde_frac,
+          balanca,
+          consumacao,
+          vende_combo,
+          vende_web,
+          grupo_id,
+          loja_id,
+          rede_id,
+          local_producao_id
+        ) values
+        (
+          source.material_id,
+          source.bn_ativo,
+          @getDate,
+          source.nu_codigo,
+          source.nm_descricao,
+          source.nm_descricaotouch,
+          source.nm_descricaoproducao,
+          source.nm_teclaprog,
+          source.nm_figura,
+          source.cod_externo,
+          source.unid_venda,
+          source.bn_venda,
+          source.bn_servico,
+          source.bn_requerobs,
+          source.bn_qtdefrac,
+          source.bn_balanca,
+          source.bn_consumacao,
+          0, --source.bn_vendenocombo,
+          0,
+          source.grupo_id,
+          @loja_id,
+          @rede_id,
+          source.st_localimpressao
+        )
+      when not matched by source then
+        update set target.ativo = 0;
+    end
 
     set identity_insert dbo.material off
   end try
