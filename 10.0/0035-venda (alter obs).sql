88e5efcc285571362a311ad0fec7e823a9e5141f
Douglas Cunha <douglas.cunha@ncr.com>
2019-08-16 10:40:52 -0300

    DSK-5331: Aumentando o tamanho das observações para 2k caracteres.

---
+++ 10.0/0035-venda (alter obs).sql
@@ -0,0 +1,7 @@
+alter table dbo.venda
+	alter column obs varchar(2000) null
+go
+
+alter table dbo.venda_geral
+	alter column obs varchar(2000) null
+go
---
+++ 10.0/0036-venda_item (alter observacoes).sql
@@ -0,0 +1,7 @@
+alter table dbo.venda_item
+	alter column observacoes varchar(2000) null
+go
+
+alter table dbo.venda_item_geral
+	alter column observacoes varchar(2000) null
+go
