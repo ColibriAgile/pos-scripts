f92c11aed33daefecbf2221cc390ada9d4455f9f
Andre <andre.foresti@ncr.com>
2019-09-24 19:04:11 -0300

    DSK-5336 Campo para dados_adicionais retornados dos plugins

---
+++ 10.0/0041-movimento_caixa(+dados_adicionais).sql
@@ -0,0 +1,12 @@
+if dbo.fn_existe('dbo.movimento_caixa.dados_adicionais') = 1
+  return
+
+alter table movimento_caixa
+add dados_adicionais varchar(1000) null
+go
+
+if dbo.fn_existe('dbo.movimento_caixa_geral.dados_adicionais') = 1
+  return
+
+alter table movimento_caixa_geral
+add dados_adicionais varchar(1000) null
