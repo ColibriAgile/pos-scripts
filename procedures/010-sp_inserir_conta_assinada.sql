e05906137ac91e0ff415e1c98252b35c7037084a
Pitty <pitty.2000@gmail.com>
2019-10-16 15:45:44 -0300

    Money para decimal(19,4)

--- procedures/010-sp_inserir_conta_assinada.sql
+++ procedures/010-sp_inserir_conta_assinada.sql
@@ -2,15 +2,22 @@ if object_id ('sp_inserir_conta_assinada') is not null
   drop procedure sp_inserir_conta_assinada
 go
 
-create procedure sp_inserir_conta_assinada (@clienteId uniqueidentifier, @operacaoId uniqueidentifier, @valor money, @dtContabil datetime, @movimentoId int)
+create procedure sp_inserir_conta_assinada 
+(
+  @clienteId uniqueidentifier, 
+  @operacaoId uniqueidentifier, 
+  @valor decimal(19,4), 
+  @dtContabil datetime, 
+  @movimentoId int
+)
 as
 begin
   begin tran
  
   declare 
-    @saldo money = dbo.fn_saldo_cliente(@clienteId, default) + @valor;
+    @saldo decimal(19, 4) = dbo.fn_saldo_cliente(@clienteid, default) + @valor;
 
-  insert into pendura 
+  insert into dbo.pendura 
   (
     cliente_id, 
     dt_hr_pendura, 
