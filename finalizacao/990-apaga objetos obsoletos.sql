31bda0a5725a04c1ee4822255370f9f13b6e764f
Andre <andre.foresti@ncr.com>
2020-02-24 14:24:01 -0300

    CFG-206 remove índice estranho da ma.terminal

--- finalizacao/990-apaga objetos obsoletos.sql
+++ finalizacao/990-apaga objetos obsoletos.sql
@@ -250,6 +250,8 @@ go
 execute sp_apagar_constraint 'un_ponto_venda$nome'
 execute sp_apagar_constraint 'ix_ponto_venda$nome'
 go
+execute sp_apagar_constraint 'ix_tecla_material$tecla$material'
+go
 
 /**************************************
  *** Campos
