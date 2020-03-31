6f49fc7e2ec9ae51788880f8da108a9aca5d6508
Pitty <pitty.d@ncr.com>
2020-03-31 13:22:54 -0300

    Endentação

--- export/007 - view classe.sql
+++ export/007 - view classe.sql
@@ -1,16 +1,24 @@
 ﻿if object_id(N'export.classe', N'V') is not null
-	drop view export.classe
+  drop view export.classe
 go
 
 create view [export].[classe]
 as
 select
-	id=id,
-	ativo=1,	
-	descricao=case when descricao <> '' then descricao else 'CLASSE ' + cast(id as varchar(5)) end,
-	descricao_touch=case when descricao_touch <> '' then descricao_touch else case 
-    when descricao <> '' then descricao else 'CLASSE ' + cast(id as varchar(5)) end end,
-	ordem_alfabetica=ordem_alfabetica,
-	imagem=imagem
+  id = id,
+  ativo = 1,  
+  descricao = case 
+    when descricao <> '' then descricao 
+    else 'CLASSE ' + cast(id as varchar(5)) 
+  end,
+  descricao_touch = case 
+    when descricao_touch <> '' then descricao_touch 
+    else case 
+      when descricao <> '' then descricao 
+      else 'CLASSE ' + cast(id as varchar(5)) 
+    end 
+  end,
+  ordem_alfabetica = ordem_alfabetica,
+  imagem = imagem
 from [dbo].[classe]
 GO
