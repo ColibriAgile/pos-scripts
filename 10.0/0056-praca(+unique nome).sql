b7ac90261927a2e04b2914f2698c63fde53ff149
Pitty <pitty.d@ncr.com>
2020-06-10 21:00:06 -0300

    Erro na criaçao do indice unico para praça

--- 10.0/0056-praca(+unique nome).sql
+++ 10.0/0056-praca(+unique nome).sql
@@ -1,4 +1,4 @@
--- exclui praças duplicadas.
+-- exclui pracas duplicadas.
 with pracas as 
 (
   select 
@@ -7,15 +7,6 @@ with pracas as
     row_num = row_number() over (partition by nome order by id, nome)
   from praca
 )
-delete from pracas
+delete pracas
 where row_num > 1;
-
-go;
-
-if exists(
-  select * from sys.indexes 
-  where name='ix_praca$nome' and object_id = object_id('praca')
-) return
-
-alter table praca 
-add constraint ix_praca$nome unique (nome)
+go
