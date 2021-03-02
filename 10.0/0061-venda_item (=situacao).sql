4341fb1ea515d700af60f602df60a2ce9eeed0e8
Pitty <pitty.d@ncr.com>
2021-03-02 16:34:14 -0300

    faltou a tabela venda_item_geral

--- 10.0/0061-venda_item (=situacao).sql
+++ 10.0/0061-venda_item (=situacao).sql
@@ -1,4 +1,3 @@
-
 if (dbo.fn_existe('dbo.venda_item.situacao') = 1)
   alter table dbo.venda_item
   drop column situacao
@@ -18,3 +17,21 @@ add situacao as
 )
 go 
 
+if (dbo.fn_existe('dbo.venda_item_geral.situacao') = 1)
+  alter table dbo.venda_item_geral
+  drop column situacao
+go
+
+alter table dbo.venda_item_geral
+add situacao as
+(
+  case [codigo_situacao] 
+    when 'N' then 'normal' 
+    when 'A' then 'cancelado antes da produção' 
+    when 'T' then 'transferido' 
+    when 'D' then 'cancelado depois da produção' 
+    when 'E' then 'estornado'
+    else 'desconhecida'
+  end
+)
+go 
