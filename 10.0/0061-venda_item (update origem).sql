905f7532cda5fd089a7836b6751efb915abe0a9f
Pitty <pitty.d@ncr.com>
2021-03-02 16:45:55 -0300

    Simplificacao do comando

--- 10.0/0061-venda_item (update origem).sql
+++ 10.0/0061-venda_item (update origem).sql
@@ -1,13 +1,11 @@
-update item
-set item.origem = venda.origem 
-from dbo.venda_item as item
-  inner join dbo.venda as venda
-    on item.venda_id=venda.venda_id
+update dbo.venda_item
+set origem = v.origem
+from dbo.venda v 
+where v.venda_id = venda_item.venda_id
 go
 
-update item
-set item.origem = venda.origem 
-from dbo.venda_item_geral as item
-  inner join dbo.venda_geral as venda
-    on item.venda_id=venda.venda_id
+update dbo.venda_item_geral
+set origem = v.origem
+from dbo.venda_geral v 
+where v.venda_id = venda_item_geral.venda_id
 go
