4ea53fb29dd89dcbbb5249f9320e23335c34523d
Andre Foresti <andre.foresti@ncr.com>
2021-03-02 10:40:27 -0300

    campo retirada na venda_geral e no ticket

--- 10.0/0058-venda(+retirada).sql
+++ 10.0/0058-venda(+retirada).sql
@@ -3,4 +3,9 @@ if (dbo.fn_existe('dbo.venda.retirada') = 1)
 
 alter table dbo.venda add
   retirada bit not null default 0
-go
+
+alter table dbo.venda_geral add
+  retirada bit not null default 0
+
+alter table dbo.ticket add
+  retirada bit not null default 0
